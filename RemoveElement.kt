2b2ac721ad6597201b6b643af87b733c4d1e6be5
Desmond Ngwuta Chidiebere <desmondmuqsit@yahoo.com>
2021-05-03 08:43:00 +0100

    Create RemoveElement.kt
    https://leetcode.com/problems/remove-element/

---
+++ RemoveElement.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun removeElement(nums: IntArray, `val`: Int): Int {
+        if(nums.isEmpty()) return 0
+        var i = 0
+        for(j in nums.indices){
+            if(nums[j] != `val`){
+                nums[i] = nums[j]
+                i++
+            }
+        }
+        return i
+    }
+}
